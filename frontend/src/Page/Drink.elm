4837822afb044933c4e2ce37a198c823c4b53080
Vaughan Kitchen <v.kitchen@gnoms.org>
2017-06-10 17:28:01 +1200

    Beautify the drinks page a little

--- frontend/src/Page/Drink.elm
+++ frontend/src/Page/Drink.elm
@@ -3,7 +3,8 @@ module Page.Drink exposing (view, update, Model, Msg, init)
 {-| The homepage.
 -}
 
-import Data.Drink exposing (Drink, DrinkIngredient)
+import Data.Drink as Drink exposing (Drink, DrinkIngredient)
+import Data.Image as Image
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Http
@@ -43,31 +44,31 @@ init slug =
 
 view : Model -> Html Msg
 view model =
-    div [ class "home-page" ]
-        [ div [ class "container page" ]
-            [ div [ class "row" ]
-                [ div [ class "col-md-9" ]
-                    [ h1 [] [ text model.drink.name ]
-                    , p [] [ text ("Drinkware: " ++ model.drink.drinkware) ]
-                    , p [] [ text ("Serve: " ++ model.drink.serve) ]
-                    , p [] [ text ("Garnish: " ++ if model.drink.garnish == "" then "None" else model.drink.garnish) ]
-                    , ul []
-                        (List.map (\v -> li [] [ viewIngredient v ]) model.drink.ingredients)
-                    , p [] [ text model.drink.method ]
-                    ]
-                , div [ class "col-md-3" ]
-                    [ div [ class "sidebar" ]
-                        [ p [] [ text "Popular Tags" ]
-                        ]
-                    ]
+  div [ class "content" ]
+    [ div [ class "content-inner" ]
+        [ div [ class "content-title" ]
+            [ h3 [] [ text model.drink.name ] ]
+        , div [ class "drink" ]
+            [ div [ class "drink-img" ]
+                [ case List.head model.drink.img of
+                    Nothing ->
+                      img [ src (Image.missing model.drink.drinkware) ]
+                        []
+                    Just img_ ->
+                      img [ src ("/img/" ++ img_) ]
+                        []
+                ]
+            , div [ class "recipe" ]
+                [ p [] [ text ("Drinkware: " ++ model.drink.drinkware) ]
+                , p [] [ text ("Serve: " ++ model.drink.serve) ]
+                , p [] [ text ("Garnish: " ++ if model.drink.garnish == "" then "None" else model.drink.garnish) ]
+                , ul []
+                    (List.map (\v -> li [] [ text (Drink.renderIngredient v) ]) model.drink.ingredients)
+                , p [] [ text model.drink.method ]
                 ]
             ]
         ]
-
-
-viewIngredient : DrinkIngredient -> Html Msg
-viewIngredient v =
-  text (v.measure ++ " " ++ v.unit ++ " " ++ v.name)
+    ]
 
 
 -- UPDATE --
